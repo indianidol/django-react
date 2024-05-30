db02fbfdad47da6a330fd1bbb8299837f6095ad0
indianidol <kartikraman@gmail.com>
2024-05-30 07:29:32 -0500

    updated all code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,7 +94,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
+
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -105,6 +114,7 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT"),
     }
 }
+
 
 
 # Password validation
